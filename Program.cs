50912b55560aa18814433970767016b4575ac649
Kariny <kariny_moreira@yahoo.com.br>
2022-07-20 21:16:54 -0300

    Bootcamp DIO .NET

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DIO_RPG.src.Entities;
+
+namespace DIO_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jenica", 23, "White Wizard");
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(7));
+        }
+    }
+}
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIO_RPG.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + " Lançou Magia ";
+        }
+
+        public string Attack(int Bonus)
+        {
+            if (Bonus > 6)
+            {
+                return this.Name + " Lançou Magia super efetiva com bonus  " + Bonus;
+            }
+            else
+            {
+                return this.Name + " Lançou uma magia com força fraca com bonus de " + Bonus;
+            }
+        }
+    }
+}
